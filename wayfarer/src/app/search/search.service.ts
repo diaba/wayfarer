4d70234060fc355c72cfa548a11ead3e6277a802
diaba <hwar@cincinnatistate.edu>
2022-01-25 11:40:50 -0500

    Added minor styling

--- wayfarer/src/app/search/search.service.ts
+++ wayfarer/src/app/search/search.service.ts
@@ -9,7 +9,7 @@ export class SearchService {
   constructor(private http: HttpClient) { }
 
   createAPIObservable(city:string, code:string){
-    return      this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},${code}&APPID=e0449c7136f994da0fd5775049e4825c`)
+    return      this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},${code}&APPID=e0449c7136f994da0fd5775049e4825c&units=imperial `)
 
   }
 }
